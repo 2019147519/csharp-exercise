7847a96e7b7af66b1c197f09c0fa71e3a9fcd845
2019147519 <songsy0418@gmail.com>
2026-01-05 16:54:24 +0900

    ..

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Reflection.Emit;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("안");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("안녕");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("안녕하");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("안녕하세");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("안녕하세요");
+            Thread.Sleep(1000);
+        }
+    }
+}
