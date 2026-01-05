99911e8aed8d773b008496095bc5bd0fac0c6375
2019147519 <songsy0418@gmail.com>
2026-01-05 10:11:02 +0900

    ..Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string menu1 = "START GAME";
+            string menu2 = "OPTIONS";
+            string menu3 = "ACHIEVEMENTS";
+            string menu4 = "EXTRAS";
+            string menu5 = "QUIT GAME";
+            
+            Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+            Console.WriteLine("┃  ██╗  ██╗ ██████╗ ██╗     ██╗      ██████╗ ██╗    ██╗     ██╗  ██╗███╗   ██╗██╗ ██████╗ ██╗  ██╗████████╗  ┃");
+            Console.WriteLine("┃  ██║  ██║██╔═══██╗██║     ██║     ██╔═══██╗██║    ██║     ██║ ██╔╝████╗  ██║██║██╔════╝ ██║  ██║╚══██╔══╝  ┃");
+            Console.WriteLine("┃  ███████║██║   ██║██║     ██║     ██║   ██║██║ █╗ ██║     █████╔╝ ██╔██╗ ██║██║██║  ███╗███████║   ██║     ┃");
+            Console.WriteLine("┃  ██╔══██║██║   ██║██║     ██║     ██║   ██║██║███╗██║     ██╔═██╗ ██║╚██╗██║██║██║   ██║██╔══██║   ██║     ┃");
+            Console.WriteLine("┃  ██║  ██║╚██████╔╝███████╗███████╗╚██████╔╝╚███╔███╔╝     ██║  ██╗██║ ╚████║██║╚██████╔╝██║  ██║   ██║     ┃");
+            Console.WriteLine("┃  ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝ ╚═════╝  ╚══╝╚══╝      ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝     ┃");
+            Console.WriteLine("┃                                                                                                            ┃");
+            Console.WriteLine("┃                                                                                                            ┃");
+            Console.WriteLine("┃                                                                                                            ┃");
+            Console.WriteLine("┃                                                                                                            ┃");
+            Console.WriteLine($"┃                                                  {menu1}                                                ┃");
+            Console.WriteLine($"┃                                                  {menu2}                                                   ┃");
+            Console.WriteLine($"┃                                                  {menu3}                                              ┃");
+            Console.WriteLine($"┃                                                  {menu4}                                                    ┃");
+            Console.WriteLine($"┃                                                  {menu5}                                                 ┃");
+            Console.WriteLine("┃                                                                                                            ┃");
+            Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
+        }
+    }
+}
